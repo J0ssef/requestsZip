85ec62354ee8661a9c9c9d833694954c10ced1e8
Jossef Harush Kadouri <128365955+J0ssef@users.noreply.github.com>
2023-03-21 15:44:19 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,133 @@
+![banner-01](https://user-images.githubusercontent.com/40632486/159684962-ae198561-16dc-4216-9f26-b223e8d01df9.png)
+
+[![Version](https://img.shields.io/pub/v/requests?include_prereleases)](https://pub.dev/packages/requests)
+[![License](https://img.shields.io/github/license/jossef/requests)](https://github.com/jossef/requests)
+
+a dart library to make HTTP requests (inspired by python [requests](https://github.com/psf/requests) module). It comes with JSON support and a lightweight implementation to store cookies like a browser.
+
+### Cookies, huh?
+Server side cookies (via response header `SET-COOKIE`) are stored using the assistance of [`quiver.cache`](https://pub.dev/documentation/quiver/latest/quiver.cache/quiver.cache-library.html). Stored cookies will be send seamlessly on the next http requests you make to the same domain (simple implementation, similar to a web browser).
+
+
+## Install
+
+Add this to your package's pubspec.yaml file:
+
+```yaml
+dependencies:
+  requests: ^4.8.0-alpha.0
+```
+
+## Usage
+Start by importing the library
+```dart
+import 'package:requests/requests.dart';
+```
+
+Let's make a simple HTTP request
+
+```dart
+var r = await Requests.get('https://google.com');
+r.raiseForStatus();
+String body = r.content();
+```
+
+### the `Response` object
+just like in python's request module, the `Response` object has this functionality
+
+- `r.throwForStatus()` - will throw an exception if the response `statusCode` is not a great success.
+- `r.raiseForStatus()` - same as `throwForStatus`
+- `r.statusCode` - the response status code
+- `r.url` - the url in the request 
+- `r.headers` - the response headers 
+- `r.success` - a boolean. `true` indicates that the request was a great success 
+- `r.hasError` - a boolean. `true` indicates that the request was not a great success 
+- `r.bytes()` - return the body in the response as a list of bytes 
+- `r.content()` - return the body in the response as a string
+- `r.json()` - recodes the body in the response and returns the result (dynamic type)
+
+
+### Optional Arguments
+
+- `json` - a `dynamic` object that will be json encoded and then be set as the request's body
+- `body` - a raw string to be used as the request's body
+- `bodyEncoding` - default `RequestBodyEncoding.FormURLEncoded`. will set the `content-type` header
+- `headers` - `Map<String, String>` of custom client headers to add in the request
+- `timeoutSeconds` - default `10` seconds. after that period of time without server response an exception is thrown
+- `persistCookies` - default `true`. if should respect server's command to persist cookie
+- `verify` - default `true`. if the SSL verification enabled
+- `withCredentials` - default `false`. for dart web to handle cookies, authorization headers, or TLS client certificates
+
+> **Note**:
+> Only one optional argument can be used in a single request `body` or `json`
+
+### Class Methods
+
+- `.clearStoredCookies(url)` - clears the stored cookies for the url
+- `.setStoredCookies(url, CookieJar)` - set the stored cookies for the url
+- `.getStoredCookies(url)` - returns a CookieJar of the stored cookies for the url
+- `.addCookie(url, name, value)` - add a Cookie to the CookieJar associated to the url
+
+ 
+## Examples
+ 
+HTTP post, body encoded as application/x-www-form-urlencoded, parse response as json
+
+```dart
+var r = await Requests.post(
+  'https://reqres.in/api/users',
+  body: {
+    'userId': 10,
+    'id': 91,
+    'title': 'aut amet sed',
+  },
+  bodyEncoding: RequestBodyEncoding.FormURLEncoded);
+
+r.raiseForStatus();
+dynamic json = r.json();
+print(json!['id']);
+```
+
+---
+
+HTTP delete
+
+```dart
+var r = await Requests.delete('https://reqres.in/api/users/10');
+r.raiseForStatus();
+```
+
+---
+
+Ignore SSL self-signed certificate
+
+```dart
+var r = await Requests.get('https://expired.badssl.com/', verify: false);
+r.raiseForStatus();
+``` 
+
+---
+
+Play with stored cookies
+
+```dart
+String url = "https://example.com";
+await Requests.clearStoredCookies(url);
+
+// Set cookies using [CookieJar.parseCookiesString]
+var cookies = CookieJar.parseCookiesString("name=value");
+await Requests.setStoredCookies(url, cookies);
+
+// Add single cookie using [CookieJar.parseCookiesString]
+var cookieJar = await Requests.getStoredCookies(url);
+cookieJar["name"] = Cookie("name", "value");
+await Requests.setStoredCookies(url, cookieJar);
+
+// Add a single cookie using [Requests.addCookie]
+// Same as the above one but without exposing `cookies.dart`
+Requests.addCookie(url, "name", "value");
+```
+
+More examples can be found in [example/](./example/).
+
+<a href="https://www.freepik.com/free-photos-vectors/business">Business vector created by freepik - www.freepik.com</a>
---
+++ test/requests_test.dart
@@ -0,0 +1,300 @@
+import 'package:http/http.dart';
+
+import 'package:requests/requests.dart';
+import 'package:requests/src/response.dart';
+import 'package:requests/src/common.dart';
+import 'package:requests/src/cookie.dart';
+import 'package:test/test.dart';
+
+void _validateResponse(Response r) {
+  expect(r.statusCode, isA<int>());
+  expect(r.hasError, isA<bool>());
+  expect(r.success, isA<bool>());
+}
+
+void main() {
+  group('A group of tests', () {
+    final String PLACEHOLDER_PROVIDER = 'https://reqres.in';
+
+    test('plain http get', () async {
+      var r = await Requests.get('https://google.com');
+      r.raiseForStatus();
+      dynamic body = r.content();
+      expect(body, isNotNull);
+      _validateResponse(r);
+    });
+
+    test('plain http get with query parameters', () async {
+      var r = await Requests.get('https://google.com',
+          queryParameters: {'id': 1, 'name': null});
+      r.raiseForStatus();
+      dynamic body = r.content();
+      expect(body, isNotNull);
+      expect(r.url.toString(), contains('?id=1'));
+      _validateResponse(r);
+    });
+
+    test('plain http get with port 80', () async {
+      var r = await Requests.get('http://google.com', port: 80);
+      r.raiseForStatus();
+      _validateResponse(r);
+      dynamic body = r.content();
+      expect(body, isNotNull);
+    });
+
+    test('plain http get with port 8080', () async {
+      var r =
+          await Requests.get('http://portquiz.net:8080/', timeoutSeconds: 30);
+      r.raiseForStatus();
+    });
+
+    test('json http get list of objects', () async {
+      var r = await Requests.get('$PLACEHOLDER_PROVIDER/api/users');
+      r.raiseForStatus();
+      dynamic body = r.json();
+      expect(body, isNotNull);
+      expect(body['data'], isList);
+      _validateResponse(r);
+    });
+
+    test('FormURLEncoded http post', () async {
+      var r = await Requests.post('$PLACEHOLDER_PROVIDER/api/users',
+          body: {
+            'userId': 10,
+            'id': 91,
+            'title': 'aut amet sed',
+            'body':
+                'libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat',
+          },
+          bodyEncoding: RequestBodyEncoding.FormURLEncoded);
+      r.raiseForStatus();
+      dynamic body = r.json();
+      expect(body, isNotNull);
+      _validateResponse(r);
+    });
+
+    test('http post a list of object', () async {
+      var r = await Requests.post('$PLACEHOLDER_PROVIDER/api/users', json: [
+        {
+          'userId': 10,
+          'id': 91,
+          'title': 'aut amet sed',
+          'body':
+              'libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat',
+        }
+      ]);
+      r.raiseForStatus();
+      dynamic body = r.json();
+      expect(body, isNotNull);
+      _validateResponse(r);
+    });
+
+    test('json http delete with request body', () async {
+      var r = await Requests.delete(
+        '$PLACEHOLDER_PROVIDER/api/users/10',
+        json: {'something': 'something'},
+      );
+      r.raiseForStatus();
+      _validateResponse(r);
+    });
+
+    test('json http post', () async {
+      var r = await Requests.post('$PLACEHOLDER_PROVIDER/api/users', json: {
+        'userId': 10,
+        'id': 91,
+        'title': 'aut amet sed',
+        'body':
+            'libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat',
+      });
+      r.raiseForStatus();
+      dynamic body = r.json();
+      expect(body, isNotNull);
+      _validateResponse(r);
+    });
+
+    test('json http delete', () async {
+      var r = await Requests.delete('$PLACEHOLDER_PROVIDER/api/users/10');
+      r.raiseForStatus();
+      _validateResponse(r);
+    });
+
+    test('json http post as a form and as a JSON', () async {
+      var r = await Requests.post('$PLACEHOLDER_PROVIDER/api/users', json: {
+        'userId': 10,
+        'id': 91,
+        'title': 'aut amet sed',
+        'body':
+            'libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat',
+      });
+      r.raiseForStatus();
+      dynamic body = r.json();
+      expect(body['userId'], 10);
+      _validateResponse(r);
+    });
+
+    test('json http get object', () async {
+      var r = await Requests.get('$PLACEHOLDER_PROVIDER/api/users/2');
+      r.raiseForStatus();
+      dynamic body = r.json();
+      expect(body, isNotNull);
+      expect(body, isMap);
+      _validateResponse(r);
+    });
+
+    test('remove cookies', () async {
+      String url = '$PLACEHOLDER_PROVIDER/api/users/1';
+      await Requests.clearStoredCookies(url);
+      var cookies = CookieJar.parseCookiesString("session=bla");
+      await Requests.setStoredCookies(url, cookies);
+      cookies = await Requests.getStoredCookies(url);
+      expect(cookies.keys.length, 1);
+      await Requests.clearStoredCookies(url);
+      cookies = await Requests.getStoredCookies(url);
+      expect(cookies.keys.length, 0);
+    });
+
+    test('add cookies', () async {
+      String url = 'http://example.com';
+      await Requests.addCookie(url, 'name', 'value');
+      var cookies = await Requests.getStoredCookies(url);
+      expect(cookies.keys.length, 1);
+      await Requests.addCookie(url, 'name', 'value');
+      cookies = await Requests.getStoredCookies(url);
+      expect(cookies.keys.length, 1);
+      await Requests.addCookie(url, 'another name', 'value');
+      cookies = await Requests.getStoredCookies(url);
+      expect(cookies.keys.length, 2);
+      await Requests.clearStoredCookies(url);
+      cookies = await Requests.getStoredCookies(url);
+      expect(cookies.keys.length, 0);
+    });
+
+    test('response as Response object', () async {
+      var r = await Requests.post('$PLACEHOLDER_PROVIDER/api/users',
+          body: {'name': 'morpheus'});
+      r.raiseForStatus();
+      var content = r.content();
+      var json = r.json();
+      expect(content, isNotNull);
+      expect(json, isNotNull);
+      _validateResponse(r);
+    });
+
+    test('throw error', () async {
+      try {
+        var r = await Requests.get('$PLACEHOLDER_PROVIDER/api/unknown/23');
+        r.raiseForStatus();
+      } on HTTPException catch (e) {
+        expect(e.response, isA<Response>());
+        expect(e.response.success, false);
+        return;
+      }
+      throw Exception('Expected request error');
+    });
+
+    test('throw if both json and body used', () async {
+      try {
+        await Requests.post('$PLACEHOLDER_PROVIDER/api/unknown/23',
+            body: {}, json: {});
+      } on ArgumentError catch (_) {
+        return;
+      }
+      throw Exception('Expected request error');
+    });
+
+    test('ssl should fail due to expired certificate', () async {
+      try {
+        var r = await Requests.get('https://expired.badssl.com/');
+        r.raiseForStatus();
+      } on Exception catch (_) {
+        return;
+      }
+
+      throw Exception('Expected ssl error');
+    });
+
+    test('ssl allow invalid', () async {
+      var r = await Requests.get('https://expired.badssl.com/', verify: false);
+      r.raiseForStatus();
+    });
+
+    test('multiple Set-Cookie response header', () async {
+      var r = await Requests.get("http://samesitetest.com/cookies/set");
+      var cookies = await Requests.extractResponseCookies(r.headers);
+
+      expect(
+        cookies["StrictCookie"]!.output(),
+        "Set-Cookie: StrictCookie=Cookie set with SameSite=Strict; path=/; httponly; samesite=strict",
+      );
+      expect(
+        cookies["LaxCookie"]!.output(),
+        "Set-Cookie: LaxCookie=Cookie set with SameSite=Lax; path=/; httponly; samesite=lax",
+      );
+      expect(
+        cookies["SecureNoneCookie"]!.output(),
+        "Set-Cookie: SecureNoneCookie=Cookie set with SameSite=None and Secure; path=/; secure; httponly; samesite=none",
+      );
+      expect(
+        cookies["NoneCookie"]!.output(),
+        "Set-Cookie: NoneCookie=Cookie set with SameSite=None; path=/; httponly; samesite=none",
+      );
+      expect(
+        cookies["DefaultCookie"]!.output(),
+        "Set-Cookie: DefaultCookie=Cookie set without a SameSite attribute; path=/; httponly",
+      );
+    });
+
+    test('cookie parsing', () async {
+      var headers = Map<String, String>();
+      var cookiesString = """
+        session=mySecret; path=/myPath; expires=Xxx, x-x-x x:x:x XXX,
+        data=1=2=3=4; _ga=GA1.4..1563550573; ; ; ; textsize=NaN; tp_state=true; _ga=GA1.3..1563550573,
+        __browsiUID=03b1cb22-d18d-&{"bt":"Browser","os":"Windows","osv":"10.0","m":"Desktop|Emulator","v":"Unknown","b":"Chrome","p":2},
+        _cb_ls=1; _cb=CaBNIWCf-db-3i9ro; _chartbeat2=..414141414.1..1; AMUUID=%; _fbp=fb.2..,
+        adblockerfound=true 
+      """;
+      headers['set-cookie'] = cookiesString;
+      var cookies = await Requests.extractResponseCookies(headers);
+
+      expect(
+        cookies["session"]!.output(),
+        "Set-Cookie: session=mySecret; path=/myPath; expires=Xxx, x-x-x x:x:x XXX",
+      );
+
+      expect(
+        cookies['data']!.output(),
+        "Set-Cookie: data=1=2=3=4",
+      );
+
+      expect(
+        cookies['__browsiUID']!.output(),
+        'Set-Cookie: __browsiUID=03b1cb22-d18d-&{"bt":"Browser","os":"Windows","osv":"10.0","m":"Desktop|Emulator","v":"Unknown","b":"Chrome","p":2}',
+      );
+
+      expect(
+        cookies['_cb_ls']!.output(),
+        "Set-Cookie: _cb_ls=1",
+      );
+
+      expect(
+        cookies['adblockerfound']!.output(),
+        "Set-Cookie: adblockerfound=true",
+      );
+    });
+
+    test('from json', () async {
+      expect(Common.fromJson('{"a":1}'), {"a": 1});
+      expect(Common.fromJson(null), null);
+    });
+
+    test('Common.getHostname', () {
+      var url = PLACEHOLDER_PROVIDER;
+      var host = Common.getHostname(url);
+      expect(host, 'reqres.in');
+      expect(Common.getHostname(host), 'reqres.in');
+      expect(Common.getHostname('$host/?=test'), 'reqres.in');
+      expect(Common.getHostname('$host:/?=test'), 'reqres.in');
+      expect(Common.getHostname('$host:8080/?=test'), 'reqres.in');
+    });
+  });
+}
